6e88311ba146124efcfc228e61052042b24b1011
Ben <ben@ben-dale.co.uk>
2020-04-21 07:40:25 +0100

    add router and basic unit testing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <div id="app" class="container">
-    <div class="row">
-      <IndexTitle />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/avalon">Avalon</router-link>
     </div>
-    <div class="row">
-      <GameOption name="The Resistance: Avalon" description="Avalon pits the forces of Good and Evil in a battle to control the future of civilization." :minPlayers="5" :maxPlayers="12" :teams="2" />
-    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import IndexTitle from './components/index/IndexTitle.vue'
-import GameOption from './components/index/GameOption.vue'
-
-export default {
-  name: 'App',
-  components: {
-    IndexTitle,
-    GameOption
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="container">
+    <div class="row">
+      <IndexTitle />
+    </div>
+    <div class="row">
+      <GameOption name="The Resistance: Avalon" description="Avalon pits the forces of Good and Evil in a battle to control the future of civilization." :minPlayers="5" :maxPlayers="12" :teams="2" />
+    </div>
+  </div>
+</template>
+
+<script>
+import IndexTitle from '@/components/index/IndexTitle.vue'
+import GameOption from '@/components/index/GameOption.vue'
+
+export default {
+  name: 'App',
+  components: {
+    IndexTitle,
+    GameOption
+  }
+}
+</script>
