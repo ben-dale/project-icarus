ebffa9137cf9da801f7731f486212a2f3c45c4b9
Ben <ben@ben-dale.co.uk>
2020-04-24 08:03:52 +0100

    get redis persistance working for both socket.io and basic key/value writing

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Project Icarus
 
+## Redis
+
+Redis is used for both it's pub/sub capability and quick key/value storing capability.
+
+The pub/sub capability is used by socket.io as an adapter. This is entirely managed by socket.io-redis. Web sockets are tied to the server that it has initiated the TCP handshake with, so naturally aren't scalable. socket.io-redis and Redis solves this problem for us by leveraging the pub/sub capability within Redis.
+
+Redis is also used for it's key/value, to store metadata about currently running rooms, for example: who is in the room, their names, their score, their role in the game etc.
+
+```
+sudo apt install redis-server
+```
+By default Redis is only accessible from localhost, which is what we want.
+
+Check if Redis is running
+```
+sudo systemctl status redis-server
+```
+
+Ping Redis
+```
+redis-cli -h localhost ping
+```
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-var path = require('path');
-var history = require('connect-history-api-fallback');
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const path = require('path');
+const history = require('connect-history-api-fallback');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
 
-var currentRooms = {};
+const redisSocketAdapter = require('socket.io-redis');
+const io = require('socket.io')(http);
+io.adapter(redisSocketAdapter({ host: 'localhost', port: 6379 }))
+
+const redis = require('redis')
+var redisClient = redis.createClient(); // defaults a connection to localhost:6379
 
 const staticFileMiddleware = express.static(path.join(__dirname, 'public'));
 app.use(staticFileMiddleware);
@@ -22,31 +26,59 @@ io.on('connection', (socket) => {
   });
 
   socket.on('avalon-start-new-game', (data) => {
-    var roomId = generateRoomId();
+    let roomId = generateRoomId();
     joinRoom(socket, roomId);
     socket.emit('avalon-room-created', { id: roomId })
   });
 
   socket.on('join-room', (data) => {
     joinRoom(socket, data.roomId, data.name);
-    io.in(data.roomId).emit('member-joined', { members: currentRooms[data.roomId].members });
-  })
+  });
 });
 
-function joinRoom(socket, roomId, name) {
-  socket.join(roomId)
-  if (!currentRooms[roomId]) {
-    console.log(roomId + ' created');
-    currentRooms[roomId] = { open: true, members: {} }
-  }
-  if (name) {
-    console.log(socket.id + ' = ' + name);
-    currentRooms[roomId].members[socket.id] = { name: name };
-  }
+redisClient.on('connect', () => {
+  console.log('Redis client connected');
+});
+
+function putObjectInRedis(key, value) {
+  redisClient.set(key, JSON.stringify(value));
+  redisClient.expire(key, 86400);
 }
 
+function getObjectFromRedis(key, onSuccess, onError) {
+  console.log('getting thing from redis, ' + key)
+  redisClient.get(key, (error, result) => {
+    console.log('error = ' + error);
+    console.log('result = ' + result);
+    if (error) {
+      onError();
+    }
+    onSuccess(JSON.parse(result));
+  });
+}
+
+function joinRoom(socket, roomId, name) {
+  socket.join(roomId);
+  getObjectFromRedis(roomId, (room) => {
+    if (room && name) {
+        console.log(socket.id + ' = ' + name);
+        room.members[socket.id] = { name: name };
+        putObjectInRedis(roomId, room);
+        io.in(roomId).emit('member-joined', { members: room.members });
+    } else {
+      console.log(roomId + ' created');
+      if (name) {
+        putObjectInRedis(roomId, { open: true, members: {} })
+      } else {
+        putObjectInRedis(roomId, { open: true, members: { [socket.id]: { name: name } } })
+      }
+    }
+  }, () => { });
+}
+
+
 function generateRoomId() {
-  return Math.random().toString(36).substring(7);
+  return Math.random().toString(36).substring(10);
 }
 
 http.listen(3000);
