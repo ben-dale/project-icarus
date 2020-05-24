389e82add4d0ab7529c72efb60bcf12f1d7b94a3
Ben <ben@ben-dale.co.uk>
2020-05-24 11:39:00 +0100

    start getting tests around server socket code

--- server/avalon/AvalonSocket.test.js
+++ server/avalon/AvalonSocket.test.js
@@ -5,20 +5,57 @@ const AllPlayers = require('../common/models/AllPlayers');
 const MockRedisClient = require('../mocks/MockRedisClient');
 const MockIo = require('../mocks/MockIo')
 
-test('blah', () => {
+test('disconnect player from open game', () => {
+  // Given
   const avalonSocket = new AvalonSocket();
   const socket = { id: 'socketId' };
   const io = new MockIo();
   const redisClient = new MockRedisClient();
 
-  // const player = new Player().init('socketId', 'Ben', 'roomId');
-  // const room = new Room().init('roomId');
-  // const allPlayers = new AllPlayers().init([player]);
+  const player = new Player().init('socketId', 'Ben', 'roomId');
+  player.storeInRedis(redisClient);
 
-  // allPlayers.storeInRedis(redisClient);
-  // room.storeInRedis(redisClient);
-  // player.storeInRedis(redisClient);
+  const room = new Room().init('roomId');
+  room.storeInRedis(redisClient);
 
+  // When
+  avalonSocket.playerDisconnected(io, redisClient, socket);
 
-  // avalonSocket.playerDisconnected(io, redisClient, socket);
+  // Then
+  expect(io.messageHistory.filter(mh => mh == 'players-updated').length).toBe(1);
+  expect(io.messageHistory.filter(mh => mh == 'room-updated').length).toBe(1);
+  expect(io.inId).toBe('roomId');
+  expect(io.obj).toBeDefined();
+  
+  new Room().getFromRedis(redisClient, 'roomId', (room) => {
+    expect(room.disconnectedPlayerIds).toStrictEqual([]);
+  }, () => { });
+});
+
+test('disconnect player from closed game', () => {
+  // Given
+  const avalonSocket = new AvalonSocket();
+  const socket = { id: 'socketId' };
+  const io = new MockIo();
+  const redisClient = new MockRedisClient();
+
+  const player = new Player().init('socketId', 'Ben', 'roomId');
+  player.storeInRedis(redisClient);
+
+  const room = new Room().init('roomId');
+  room.game.closed = true;
+  room.storeInRedis(redisClient);
+
+  // When
+  avalonSocket.playerDisconnected(io, redisClient, socket);
+
+  // Then
+  expect(io.messageHistory.filter(mh => mh == 'players-updated').length).toBe(1);
+  expect(io.messageHistory.filter(mh => mh == 'room-updated').length).toBe(1);
+  expect(io.inId).toBe('roomId');
+  expect(io.obj).toBeDefined();
+
+  new Room().getFromRedis(redisClient, 'roomId', (room) => {
+    expect(room.disconnectedPlayerIds).toStrictEqual(['socketId']);
+  }, () => { });
 })
